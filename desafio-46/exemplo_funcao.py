f6b33d8194de467cd23ed943cfa66fd0eee1b571
kaique donizeti <kaiquedonizetisilva@gmail.com>
2023-10-18 21:32:40 -0300

    Add files via upload

---
+++ desafio-46/exemplo_funcao.py
@@ -0,0 +1,6 @@
+def monta_computador(cpu='', armazenamento=0, memoria=0):
+    print(f"A configuração é: - CPU: {cpu}")
+    print(f"A configuração é: - Armazenamento: {armazenamento}")
+    print(f"A configuração é: - Memória: {memoria}Gb")
+
+monta_computador('Intel Core i9', 4, 64)
